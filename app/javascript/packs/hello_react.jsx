13a2493cb09c9fa5d58dc304f2377a487137929b
Ana <anasyarifah@gmail.com>
2018-04-21 13:42:25 +0700

    replace layout with slim

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -11,7 +11,7 @@ const Hello = props => (
 )
 
 Hello.defaultProps = {
-  name: 'David'
+  name: 'DavidBOnd'
 }
 
 Hello.propTypes = {
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="DUde" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
